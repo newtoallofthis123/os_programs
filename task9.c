251b6d9c38764535287d26ad689e946c5dbdaa64
newtoallofthis123 <noobscience123@gmail.com>
2024-07-08 15:47:49 +0530

    Add task 9

---
+++ task9.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// To implement paging technique of memory management
+int main(){
+    int np, ps,pt[20],nf,la,pn,index,pa, i, j;
+    printf("Enter number of pages: \n");
+    scanf("%d", &np);
+    printf("Enter the page size: \n");
+    scanf("%d", &ps);
+    printf("Enter the number of frames: \n");
+    scanf("%d", &nf);
+    printf("Enter the page table");
+    for(i = 0; i < np; i++){
+        scanf("%d", &pt[i]);
+    }
+    printf("Enter the logical address: \n");
+    scanf("%d", &la);
+    pn = la/ps;
+    index = la%ps;
+    pa = pt[pn]+ps + index;
+    printf("The physical address is: %d\n", pa);
+}
